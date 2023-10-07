3f701477bc30efe8141b259bfc9f98f40e18af52
Saydaa <saidabally@gmail.com>
2023-10-07 10:58:40 +0200

    Registration form version 1

--- registration/register.php
+++ registration/register.php
@@ -10,13 +10,32 @@
 </head>
 
 <body>
-    <form action="">
-        <p>why is this code not showing urgh</p>
+    <form method="post" action="register.php">
+        <?php include('errors.php'); ?>
+        <div class="input-group">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" value="<?php echo $username; ?>">
+        </div>
+        <div class="input-group">
+            <label for="email">Email</label>
+            <input id="email" type="email" name="email" value="<?php echo $email; ?>">
+        </div>
+        <div class="input-group">
+            <label for="password">Password</label>
+            <input type="password" name="password_2" id="password">
+        </div>
+        <div class="input-group">
+            <label for="Confirm_password">Confirm Password</label>
+            <input type="password" name="password_2" id="Confirm_password">
+        </div>
+        <div class="input-group">
+            <button type="submit" class="btn" name="reg_user">Register</button>
+        </div>
+        <p>Already a member ? <a href="login.php">Sign in</a></p>
+
     </form>
 
-    <form action="">
-        <p>why is this code not showing urgh</p>
-    </form>
+
 
 
 </body>
